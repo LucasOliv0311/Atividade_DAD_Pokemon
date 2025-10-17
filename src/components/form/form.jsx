9c40d1a788dfca1e1b7e39f4ed80b97cf97d3a82
Lucas Oliveira <lucas.gracas@germinare.org.br>
2025-10-17 11:56:58 -0300

    criando formulário

---
+++ src/components/form/form.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import styles from './form.module.css';
+
+function Form({onSubmit}) {
+    const [name, setName] = useState("");
+    const [type, setType] = useState("");
+    const [power, setPower] = useState(0);
+
+    function submit(e) {
+        e.preventDefault();
+
+        const newPokemon = {
+            name,
+            type,
+            power: Number(power)
+        };
+        onSubmit(newPokemon);
+
+        setName("");
+        setType("");
+        setPower(0);
+    }
+
+    return (
+        <form className={styles.form} onSubmit={submit}>
+            <h1>Cadastrar Pokémon</h1>
+
+            <div className={styles.fieldContainer}>
+                <span>Nome:</span>
+
+                <input
+                    type="text"
+                    placeholder='Nome'
+                    className={styles.field}
+                    required
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+
+            <div className={styles.fieldContainer}>
+                <span>Tipo:</span>
+
+                <select
+                    className={styles.field}
+                    required
+                    onChange={(e) => setType(e.target.value)}
+                >
+                    <option value="Normal">Normal</option>
+                    <option value="Fogo">Fogo</option>
+                    <option value="ÁGua">ÁGua</option>
+                    <option value="Gelo">Gelo</option>
+                    <option value="Terrestre">Terrestre</option>
+                    <option value="Voador">Voador</option>
+                    <option value="Lutador">Lutador</option>
+                </select>
+            </div>
+
+            <div className={styles.fieldContainer}>
+                <span>Poder:</span>
+
+                <input
+                    type="number"
+                    placeholder='Poder'
+                    className={styles.field}
+                    required
+                    min={0}
+                    max={100}
+                    onChange={(e) => setPower(e.target.value)}
+                />
+            </div>
+
+            <button
+                type='submit'
+                className={styles.button}
+            >
+                Enviar
+            </button>
+        </form>
+    );
+}
+
+export default Form;
